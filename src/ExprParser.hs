ce1199bff7b6244df2cb93151d5320f4ba956dda
John Haynes <john.andrew.haynes@gmail.com>
2020-11-29 21:11:26 +0100

    wip

--- src/ExprParser.hs
+++ src/ExprParser.hs
@@ -3,17 +3,34 @@ module ExprParser where
 import Combinators
 import Core
 import LexState
-import Parser      (Parser (..), (<|>))
-import Tokens      (Token (..))
+import Parser       (Parser (..), (<|>))
+import Tokens       (Token (..))
 
-import Data.Monoid ((<>))
+import Data.Functor ((<&>))
 
+-- Lowest precedence
 parseExpr :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
-parseExpr = apply <|> nonApply
+parseExpr = (sumTerm >>= \(sp, a) -> compOp >>= \(_, op) -> parseExpr >>= \(_, b) -> pure (sp, EBinPrimOp op a b))
+        <|> sumTerm
+    where
+    compOp = eqI <|> ltEqI <|> ltI <|> gtEqI <|> gtI
 
-  where
-  apply :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
-  apply = do 
+-- Next lowest precedence
+sumTerm :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
+sumTerm = (productTerm >>= \(sp, a) -> summishOp >>= \(_, op) -> sumTerm >>= \(_, b) -> pure (sp, EBinPrimOp op a b))
+      <|> productTerm
+    where
+    summishOp = stitch <|> addI <|> subI --TODO will stitch be mistaken for +?
+
+-- etc..
+productTerm :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
+productTerm = (apply >>= \(sp, a) -> mullishOp >>= \(_, op) -> productTerm >>= \(_, b) -> pure (sp, EBinPrimOp op a b))
+          <|> apply
+    where
+    mullishOp = mulI
+
+apply :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
+apply = do
     (sp, e) <- nonApply
     es      <- map snd <$> many (nonApplyRighterThan sp)
     pure (sp, foldl EApp e es)
@@ -31,60 +48,101 @@ nonApplyRighterThan (SourcePos _ c) =
       Left _ -> Left "faily"
 
 nonApply :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
-nonApply = paren
+nonApply = eparen
        <|> err
        <|> eShow
        <|> ifThenElse
+       <|> term
        <|> lambda
        <|> elet
-       <|> term
-       <|> eBinPrimOp
        <|> (Parser $ \s -> Left $ "Could not turn into nonApply: " <> show s)
 
-  where
-  paren :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
-  paren = token LParen *> parseExpr <* token RParen
+term :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
+term = do
+    (sp, t) <- literal <|> variable <|> dCons
+    pure (sp, ETerm t)
 
-  -- Currying happens here.
-  lambda :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
-  lambda = do
+variable :: Show s => Parser [(SourcePos, Token s)] (SourcePos, Term s)
+variable = lower <&> \(sp, v) -> (sp, Var v)
+
+schoenfinkel :: [s] -> Expr s -> Expr s
+schoenfinkel vs body = foldr ELam body vs
+
+token :: Eq s => Token s -> Parser [(SourcePos, Token s)] (SourcePos, Token s)
+token x =
+    Parser $ \s -> case s of
+        []           -> Left "token: No more tokens"
+        ((sp,t):ts') -> if t == x then Right (ts', (sp, t)) else Left "token mismatch"
+
+lower :: Show s => Parser [(SourcePos, Token s)] (SourcePos, s)
+lower =
+    Parser $ \s -> case s of
+        []                       -> Left "lower: No more tokens"
+        ((sp, LowerIdent i):ts') -> Right (ts', (sp, i))
+        other                    -> Left ("Not a lower: " ++ show other)
+
+upper :: Show s => Parser [(SourcePos, Token s)] (SourcePos, s)
+upper =
+    Parser $ \s -> case s of
+        []                       -> Left "upper: No more tokens"
+        ((sp, UpperIdent i):ts') -> Right (ts', (sp, i))
+        other                    -> Left ("Not a upper: " ++ show other)
+
+literal :: Parser [(SourcePos, Token s)] (SourcePos, Term s)
+literal =
+    Parser $ \s -> case s of
+        ((sp, TokInt i):ts')     -> Right (ts', (sp, LitInt i))
+        ((sp, TokBool b):ts')    -> Right (ts', (sp, LitBool b))
+        ((sp, TokString st):ts') -> Right (ts', (sp, LitString st))
+        (_:_)                    -> Left "Not a literal (int)"
+        []                       -> Left "Out of tokens"
+
+dCons :: Show s => Parser [(SourcePos, Token s)] (SourcePos, Term s)
+dCons = upper <&> \(sp, v) -> (sp, DCons v)
+
+-- Currying happens here.
+lambda :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
+lambda = do
     (sp, _)   <- token Lambda
     params    <- map (unvar . snd) <$> many1 variable
     _         <- token Dot
     (_, body) <- parseExpr
     pure (sp, schoenfinkel params body)
 
-  -- Let/Letrec/Fix
-  elet :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
-  elet = do
-      (sp,   _) <- token TLet
-      negs      <- many1 variable
-      _         <- token SingleEq
-      (_, b)    <- parseExpr
-      _         <- token In
-      (_, c)    <- parseExpr
-      let (a:vars') = map (unvar . snd) negs
-          lam = schoenfinkel vars' b
-      pure (sp, ELet a lam c)
+-- Let/Letrec/Fix
+elet :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
+elet = do
+    (sp,   _) <- token TLet
+    negs      <- many1 variable
+    _         <- token SingleEq
+    (_, b)    <- parseExpr
+    _         <- token In
+    (_, c)    <- parseExpr
+    let (a:vars') = map (unvar . snd) negs
+        lam = schoenfinkel vars' b
+    pure (sp, ELet a lam c)
 
-  unvar :: Show s => Term s -> s
-  unvar (Var s) = s
-  unvar x = error $ "Not a var: " ++ show x
+unvar :: Show s => Term s -> s
+unvar (Var s) = s
+unvar       x = error $ "Not a var: " ++ show x
 
-  err :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
-  err = do
-      (sp, _) <- token TErr
-      ( _, e) <- nonApply
-      pure (sp, EUnPrimOp Err e)
+eparen :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
+eparen = token LParen *> parseExpr <* token RParen
 
-  eShow :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
-  eShow = do
-      (sp, _) <- token TShow
-      ( _, e) <- nonApply
-      pure (sp, EUnPrimOp EShow e)
+err :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
+err = do
+    (sp, _) <- token TErr
+    ( _, e) <- nonApply
+    pure (sp, EUnPrimOp Err e)
 
-  ifThenElse :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
-  ifThenElse = do
+eShow :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
+eShow = do
+    (sp, _) <- token TShow
+    ( _, e) <- nonApply
+    pure (sp, EUnPrimOp EShow e)
+
+ifThenElse :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
+ifThenElse = do
     (sp, _) <- token If
     (_,  a) <- parseExpr
     _       <- token Then
@@ -93,102 +151,30 @@ nonApply = paren
     (_,  c) <- parseExpr
     pure (sp, IfThenElse a b c)
 
-  eBinPrimOp :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
-  eBinPrimOp = do
-    (sp, o) <- op
-    (_, e1) <- nonApply
-    (_, e2) <- nonApply
-    pure (sp, EBinPrimOp o e1 e2)
+mulI :: Eq s => Parser [(SourcePos, Token s)] (SourcePos, BinOp)
+mulI = token Times <&> \(sp, _) -> (sp, MulI)
 
-  op :: Eq s => Parser [(SourcePos, Token s)] (SourcePos, BinOp)
-  op = addI <|> subI <|> mulI <|> eqI <|> stitch <|> ltEqI <|> ltI <|> gtEqI <|> gtI
-    where
-    addI :: Eq s => Parser [(SourcePos, Token s)] (SourcePos, BinOp)
-    addI = do
-      (sp, _) <- token Plus
-      pure (sp, AddI)
+addI :: Eq s => Parser [(SourcePos, Token s)] (SourcePos, BinOp)
+addI = token Plus <&> \(sp, _) -> (sp, AddI)
 
-    subI :: Eq s => Parser [(SourcePos, Token s)] (SourcePos, BinOp)
-    subI = do
-      (sp, _) <- token Minus
-      pure (sp, SubI)
+subI :: Eq s => Parser [(SourcePos, Token s)] (SourcePos, BinOp)
+subI = token Minus <&> \(sp, _) -> (sp, SubI)
 
-    mulI :: Eq s => Parser [(SourcePos, Token s)] (SourcePos, BinOp)
-    mulI = do
-      (sp, _) <- token Times
-      pure (sp, MulI)
+stitch :: Eq s => Parser [(SourcePos, Token s)] (SourcePos, BinOp)
+stitch = token TStitch <&> \(sp, _) -> (sp, ConcatS)
 
-    eqI :: Eq s => Parser [(SourcePos, Token s)] (SourcePos, BinOp)
-    eqI = do
-      (sp, _) <- token DoubleEq
-      pure (sp, EqI)
+eqI :: Eq s => Parser [(SourcePos, Token s)] (SourcePos, BinOp)
+eqI = token DoubleEq <&> \(sp, _) -> (sp, EqI)
 
-    stitch :: Eq s => Parser [(SourcePos, Token s)] (SourcePos, BinOp)
-    stitch = do
-      (sp, _) <- token TStitch
-      pure (sp, ConcatS)
+ltEqI :: Eq s => Parser [(SourcePos, Token s)] (SourcePos, BinOp)
+ltEqI = token TLessEq <&> \(sp, _) -> (sp, LtEqI)
 
-    ltEqI :: Eq s => Parser [(SourcePos, Token s)] (SourcePos, BinOp)
-    ltEqI = do
-      (sp, _) <- token TLessEq
-      pure (sp, LtEqI)
+ltI :: Eq s => Parser [(SourcePos, Token s)] (SourcePos, BinOp)
+ltI = token TLessThan <&> \(sp, _) -> (sp, LtI)
 
-    ltI :: Eq s => Parser [(SourcePos, Token s)] (SourcePos, BinOp)
-    ltI = do
-      (sp, _) <- token TLessThan
-      pure (sp, LtI)
+gtEqI :: Eq s => Parser [(SourcePos, Token s)] (SourcePos, BinOp)
+gtEqI = token TGreaterEq <&> \(sp, _) -> (sp, GtEqI)
 
-    gtEqI :: Eq s => Parser [(SourcePos, Token s)] (SourcePos, BinOp)
-    gtEqI = do
-      (sp, _) <- token TGreaterEq
-      pure (sp, GtEqI)
+gtI :: Eq s => Parser [(SourcePos, Token s)] (SourcePos, BinOp)
+gtI = token TGreaterThan <&> \(sp, _) -> (sp, GtI)
 
-    gtI :: Eq s => Parser [(SourcePos, Token s)] (SourcePos, BinOp)
-    gtI = do
-      (sp, _) <- token TGreaterThan
-      pure (sp, GtI)
-
-
-
-  term :: (Eq s, Show s) => Parser [(SourcePos, Token s)] (SourcePos, Expr s)
-  term = do
-    (sp, t) <- literal <|> variable <|> dCons
-    pure (sp, ETerm t)
-
-  literal :: Parser [(SourcePos, Token s)] (SourcePos, Term s)
-  literal = Parser $ \s -> case s of
-    ((sp, TokInt i):ts')    -> Right (ts', (sp, LitInt i))
-    ((sp, TokBool b):ts')   -> Right (ts', (sp, LitBool b))
-    ((sp, TokString st):ts') -> Right (ts', (sp, LitString st))
-    (_:_)                   -> Left "Not a literal (int)"
-    []                      -> Left "Out of tokens"
-
-  variable :: Show s => Parser [(SourcePos, Token s)] (SourcePos, Term s)
-  variable = do
-    (sp, v) <- lower
-    pure (sp, Var v)
-
-  dCons :: Show s => Parser [(SourcePos, Token s)] (SourcePos, Term s)
-  dCons = do
-    (sp, v) <- upper
-    pure (sp, DCons v)
-
-lower :: Show s => Parser [(SourcePos, Token s)] (SourcePos, s)
-lower = Parser $ \s -> case s of
-  []                       -> Left "lower: No more tokens"
-  ((sp, LowerIdent i):ts') -> Right (ts', (sp, i))
-  other                    -> Left ("Not a lower: " ++ show other)
-
-upper :: Show s => Parser [(SourcePos, Token s)] (SourcePos, s)
-upper = Parser $ \s -> case s of
-  []                       -> Left "upper: No more tokens"
-  ((sp, UpperIdent i):ts') -> Right (ts', (sp, i))
-  other                    -> Left ("Not a upper: " ++ show other)
-
-token :: Eq s => Token s -> Parser [(SourcePos, Token s)] (SourcePos, Token s)
-token x = Parser $ \s -> case s of
-  []           -> Left "token: No more tokens"
-  ((sp,t):ts') -> if t == x then Right (ts', (sp, t)) else Left "token mismatch"
-
-schoenfinkel :: [s] -> Expr s -> Expr s
-schoenfinkel vs body = foldr ELam body vs
